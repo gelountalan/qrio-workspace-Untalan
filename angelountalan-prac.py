37f66de3e9778456059c951d6f09d2804902e933
Angelo Untalan <angelountalan17@gmail.com>
2025-02-10 09:57:50 +0800

    test from yt

---
+++ angelountalan-prac.py
@@ -0,0 +1,41 @@
+# # variables             int = whole number, float = decimal number, string = text boolean = True or False
+# #price = 10
+# #price = 20   # eto ang lalabas kasi ito ang last na line na binasa
+
+# #print (price)
+
+# patient_name = "John Smith"
+# age = 20
+# is_new = True
+# is_old = False
+# print(patient_name)
+# print(age)
+# print(is_new)
+
+
+#name = input('what is your name? ')     #input para mapa print yung nasa loob ng ''
+#print ('Hi ' + name)
+
+
+#exercise
+
+# name = input('What is you name? ')
+# print ('hello '+ name)
+# color = input ('What is your favourite color? ') #nag dagdag ng bagong variable para malaman ang color
+# print (name +' likes the color ' +color )
+
+
+#birth_year = input ('Brith year: ')
+
+#age = 2025 - int(birth_year)    #kaya may int kase we are subtracting int '2025' sa string 'birth_year'
+#print (age)                     # int() para ma convert and string to int (text to number)
+                                #float() para ma convert string to float (text to number with decimal point)
+                                #str() para ma convert and int to string (number to text)
+                                #bool() para ma convert and string to boolean value
+                                #tuwing gagamit ng input() function, laging string ang ma babasa, if numerical value ang gusto mapa labas, tsaka gagamit ng sting or float
+
+#exercise
+weight_lbs = input ('what is your weight in lbs?: ')
+weight_kg = int(weight_lbs) * .453
+print ('Your weight in kg is: ' + str(weight_kg) + ('kg'))
+
